70215962005f37dfb861dce072ba3ee4e7c4a06a
Jason Weaver <jason.weaver@utexas.edu>
2014-07-10 01:04:38 -0500

    Stylized Paragraphs

--- testPOI/src/testPOI/Word.java
+++ testPOI/src/testPOI/Word.java
@@ -3,6 +3,8 @@ package testPOI;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.Borders;
 
 import java.io.FileOutputStream;
 
@@ -13,17 +15,23 @@ public class Word {
 		
 		XWPFParagraph paragraph = document.createParagraph();
 		XWPFRun run = paragraph.createRun();
+		paragraph.setAlignment(ParagraphAlignment.CENTER);
+		paragraph.setIndentationHanging(1000);
+		paragraph.setBorderTop(Borders.BASIC_BLACK_DASHES);
+		paragraph.setBorderBottom(Borders.BASIC_BLACK_DASHES);
+		paragraph.setSpacingAfter(1000);							// 20 = 1pt of font?
 		
-		run.setText("Pancakes");
-		run.setText(" and Peanut Butter!");
+		run.setText("Pancakes"); 
+		run.setText(" and Peanut Butter! ");
 		run.addBreak();
-		run.setText("I'm hungry dude.");	
-				
+		run.setText("I'm hungry dude. I'm hungry dude. I'm hungry dude. I'm hungry dude. I'm hungry dude."
+				+ "I'm hungry dude. I'm hungry dude. I'm hungry dude. I'm hungry dude. I'm hungry dude.");	
 		
 		XWPFParagraph paragraph2 = document.createParagraph();
-		XWPFRun run2 = paragraph2.createRun();	
+		XWPFRun run2 = paragraph2.createRun();
+		// paragraph2.setPageBreak(true); couldn't get this to work
 		
-		run2.setText("Notice the line Bread for a new paragraph.");
+		run2.setText("Notice the line break for a new paragraph.");
 
 		
 		try {
