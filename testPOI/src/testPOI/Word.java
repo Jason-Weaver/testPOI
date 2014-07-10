28e18e6d5cad5341f979b99e5db866102b408204
Jason Weaver <jason.weaver@utexas.edu>
2014-07-10 00:59:50 -0500

    Output words.

--- testPOI/src/testPOI/Word.java
+++ testPOI/src/testPOI/Word.java
@@ -1,13 +1,30 @@
 package testPOI;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import java.io.FileOutputStream;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 
+import java.io.FileOutputStream;
 
 public class Word {
 
 	public static void main(String[] args) {
 		XWPFDocument document = new XWPFDocument();
+		
+		XWPFParagraph paragraph = document.createParagraph();
+		XWPFRun run = paragraph.createRun();
+		
+		run.setText("Pancakes");
+		run.setText(" and Peanut Butter!");
+		run.addBreak();
+		run.setText("I'm hungry dude.");	
+				
+		
+		XWPFParagraph paragraph2 = document.createParagraph();
+		XWPFRun run2 = paragraph2.createRun();	
+		
+		run2.setText("Notice the line Bread for a new paragraph.");
+
 		
 		try {
 			FileOutputStream output = new FileOutputStream("Awesome.docx");
