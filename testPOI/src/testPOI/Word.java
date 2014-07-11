827d15ddb948d5a25585cdd921bfae5f65a8bbf2
Jason Weaver <jason.weaver@utexas.edu>
2014-07-11 05:19:30 -0500

    Upload Test Package with font tests

--- testPOI/src/testPOI/Word.java
+++ testPOI/src/testPOI/Word.java
@@ -6,11 +6,20 @@ import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.UnderlinePatterns;
 
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Word {
 
 	public static void main(String[] args) {
+		
+
+
+        
 		XWPFDocument document = new XWPFDocument();
 		
 		XWPFParagraph paragraph = document.createParagraph();
@@ -19,20 +28,20 @@ public class Word {
 
 		run.setText("Ag");	
 		run.setFontSize(84);
-		run.setFontFamily("Baskerville");					// don't add .ttc
-			// Fonts that Worked: Futura, Kefa, Telugu MN, Verdana, Papyrus, Noteworthy, Baskerville
-			// Fonts that didn't: PTSans, Sana, Corsiva, Ariel, Baoli
-		// run.setUnderline(UnderlinePatterns.DOUBLE);			works 
-		// run.setBold(true);									works
-		// run.setItalic(true);									works
-		// run.setStrike(true);									works
+		run.setFontFamily("PT Sans");	// must be font family name (devoid of attributes like bold)
+		
+		// run.setUnderline(UnderlinePatterns.DOUBLE);			 
+		// run.setBold(true);									
+		// run.setItalic(true);									
+		// run.setStrike(true);									
+		// System.out.println(run.getFontFamily());
 
 
 		run.setColor("0099FF");
 		
 		
 		try {
-			FileOutputStream output = new FileOutputStream("Awesome.docx");
+			FileOutputStream output = new FileOutputStream("./Awesome.docx");
 			document.write(output);
 			output.close();
 			
